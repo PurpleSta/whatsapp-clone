c340c750beae9a53a19ca6b803236f41d30f515e
lowkieman <lowkieman@gmail.com>
2024-09-06 08:19:15 +0300

    added the behavior of the search button on chats

--- js/home.js
+++ js/home.js
@@ -1,4 +1,23 @@
 const searchButton=document.getElementById("search");
 searchButton.addEventListener("focus",function(){
     document.getElementById("search").style.outline="none";
+    document.getElementById("bi-search-hide").style.display="none";
+    document.getElementById("bi-arrow-left-show").style.display="inline-block";
 });
+
+searchButton.addEventListener("blur",function(){    
+    document.getElementById("bi-search-hide").style.display="inline-block";
+    document.getElementById("bi-arrow-left-show").style.display="none";
+});
+
+const biSearchHide=document.getElementById("bi-search-hide");
+const biArrowLeftShow=document.getElementById("bi-arrow-left-show");
+biSearchHide.addEventListener("click",function(){
+    document.getElementById("bi-search-hide").style.display="none";
+    document.getElementById("bi-arrow-left-show").style.display="inline-block";
+});
+
+biArrowLeftShow.addEventListener("click",function(){
+    document.getElementById("bi-search-hide").style.display="none";
+    document.getElementById("bi-arrow-left-show").style.display="inline-block";
+})
