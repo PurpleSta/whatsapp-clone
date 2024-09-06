b5688b0e8d1a8f557f05cda61cc0928e3b24e20d
lowkieman <lowkieman@gmail.com>
2024-09-06 08:25:05 +0300

    added css for chats and js for chats

---
+++ js/chats/profile.js
@@ -0,0 +1,11 @@
+const hideArrow=document.getElementById("hideArrow");
+hideArrow.addEventListener("click",function(){
+    const profileArrow=document.getElementById("profileArrow");
+    profileArrow.style.display="none"
+})
+
+const showArrow=document.getElementById("showArrow");
+
+showArrow.addEventListener("click",function(){
+    document.getElementById("profileArrow").style.display="inline-block";
+})
